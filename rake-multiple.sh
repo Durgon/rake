768e238744cf5cde6f1101f2e558c63afa39dd2c
Durgon <jacob@hammond.cloud>
2018-11-28 10:43:28 -0700

    added --show-progress switch to wget loop. update echo messages on single

--- rake-multiple.sh
+++ rake-multiple.sh
@@ -4,7 +4,7 @@ clear
 #ask for file
 read -p "Specify the file to rake: " list
 #create the albums directory if not present
-mkdir -p albums
+mkdir -p Albums
 for line in `cat $list`; do
   #get post title for the link
   label=`wget -q "$line" -O - |grep 'post-title '| cut -d ">" -f2 | cut -d "<" -f1`
@@ -18,7 +18,7 @@ for line in `cat $list`; do
   echo ""
   echo "Raking images into bag"
   #rake through album and get all images
-  wget -q "$line" -O - |grep 'post-image-container'| cut -d '"' -f2|while read id;do echo "Raking $id.jpg";wget --progress=bar:force -e robots=off -q -c "https://i.imgur.com/$id.jpg" -P "$bag";done
-  echo ">done raking $label @ $line"
+  wget -q "$line" -O - |grep 'post-image-container'| cut -d '"' -f2|while read id;do echo "Raking $id.jpg";wget -qce robots=off --show-progress "https://i.imgur.com/$id.jpg" -P "$bag";done
+  echo "done raking $label @ $line"
 done
 echo "rake complete of $list"
--- rake-single.sh
+++ rake-single.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 clear
-read -p "Enter the imgur album link you would like to download: " album
+read -p "Enter the link to the imgur album you want to rake: " album
 mkdir -p albums
 folder=$(wget -q "$album" -O - |grep 'post-title '| cut -d ">" -f2 | cut -d "<" -f1)
-echo "Found an album called '$folder'"
+echo "Found some images to rake $folder @ $album"
 mkdir -p albums/"$folder"
-wget -e robots=off -q "$album" -O - |grep 'post-image-container'| cut -d '"' -f2|while read id;do echo "Downloading $id.jpg";wget -q -c "https://i.imgur.com/$id.jpg" -P albums/"$folder"/;done
+wget -e robots=off -q "$album" -O - |grep 'post-image-container'| cut -d '"' -f2|while read id;do echo "raking $id.jpg";wget -qce robots==off --show-progress "https://i.imgur.com/$id.jpg" -P albums/"$folder"/;done
