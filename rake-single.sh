b1dd097888f1e32d78db41c9a36d6fb5211d6c46
Durgon <jacob@hammond.cloud>
2018-12-15 11:40:50 -0700

    Added test section Tweaked echo'd text Fixed incorrect boolean on robots

--- rake-single.sh
+++ rake-single.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
 clear
-read -p "Enter the link to the imgur album you want to rake: " album
-mkdir -p albums
+#---------TESTING ONLY--------
+echo "remove existing Albums folder"
+rm -rf Albums/
+#--------END OF TESTING--------
+
+#read -p "Enter the link to the imgur album you want to rake: " album
+#
+album=$1
+mkdir -p Albums
 folder=$(wget -q "$album" -O - |grep 'post-title '| cut -d ">" -f2 | cut -d "<" -f1)
-echo "Found some images to rake $folder @ $album"
-mkdir -p albums/"$folder"
-wget -e robots=off -q "$album" -O - |grep 'post-image-container'| cut -d '"' -f2|while read id;do echo "raking $id.jpg";wget -qce robots==off --show-progress "https://i.imgur.com/$id.jpg" -P albums/"$folder"/;done
+echo "Found some images to rake from $folder @ $album"
+mkdir -p Albums/"$folder"
+count=`wget -q "$album" -O - |grep 'post-image-container'| cut -d '"' -f2|wc -l`
+echo "Found $count images to rake"
+wget -e robots=off -q "$album" -O - |grep 'post-image-container'| cut -d '"' -f2|while read id;do echo "Raking $id.jpg";wget -qce robots=off --show-progress "https://i.imgur.com/$id.jpg" -P albums/"$folder"/;done
