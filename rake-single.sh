f1b4ee99f43543ab8f92e5835bc0134ad4bfe5ac
Durgon <jacob@hammond.cloud>
2019-07-07 20:43:38 -0700

    fixed robots off boolean

--- rake-single.sh
+++ rake-single.sh
@@ -6,4 +6,4 @@ mkdir -p albums
 folder=$(wget -q "$album" -O - |grep 'post-title '| cut -d ">" -f2 | cut -d "<" -f1)
 echo "Found some images to rake $folder @ $album"
 mkdir -p albums/"$folder"
-wget -e robots=off -q "$album" -O - |grep 'post-image-container'| cut -d '"' -f2|while read id;do echo "raking $id.jpg";wget -qce robots==off --show-progress "https://i.imgur.com/$id.jpg" -P albums/"$folder"/;done
+wget -e robots=off -q "$album" -O - |grep 'post-image-container'| cut -d '"' -f2|while read id;do echo "raking $id.jpg";wget -qce robots=off --show-progress "https://i.imgur.com/$id.jpg" -P albums/"$folder"/;done
